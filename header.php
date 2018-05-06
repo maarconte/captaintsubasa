e01e26253a425a42927c8963245a476ce87f557f
Mathilde Arconte <signup@marconte.fr>
2018-05-06 13:22:57 +0200

    Header

--- header.php
+++ header.php
@@ -29,30 +29,30 @@
 <?php if (is_single() || is_page()): ?> style="background-image: url(<?php the_post_thumbnail_url()?>)" <?php endif;?> >
 	<!-- Menu -->
 	<nav id="site-navigation" class="main-navigation navbar navbar-expand-lg fixed-top" role="navigation">
-		<a class="navbar-brand" href="#">Demarquage</a>
+		<a class="navbar-brand" href="<?=home_url()?>">Demarquage</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"><i class="fas fa-bars"></i></span>
 		</button>
 		<!-- Primary Menu -->
 		<div id="bs-example-navbar-collapse-1" class="collapse navbar-collapse">
 			<?php wp_nav_menu(array(
-				'theme_location' => 'primary',
-				'depth' => 2, // 1 = no dropdowns, 2 = with dropdowns.
-				'container' => 'ul',
-				'menu_class' => 'navbar-nav ml-auto',
-				'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-				'walker' => new WP_Bootstrap_Navwalker(),
-				));?>
+    'theme_location' => 'primary',
+    'depth' => 2, // 1 = no dropdowns, 2 = with dropdowns.
+    'container' => 'ul',
+    'menu_class' => 'navbar-nav ml-auto',
+    'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
+    'walker' => new WP_Bootstrap_Navwalker(),
+));?>
 			<!-- Social Menu -->
 			<?php if (has_nav_menu('social')): ?>
 				<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e('Footer Social Links Menu', 'captaintsubasa');?>">
 					<?php wp_nav_menu(array(
-						'theme_location' => 'social',
-						'menu_class' => 'social-links-menu',
-						'depth' => 1,
-						'link_before' => '<span class="screen-reader-text">',
-						'link_after' => '</span>' . captaintsubasa_get_svg(array('icon' => 'chain')),
-					));?>
+    'theme_location' => 'social',
+    'menu_class' => 'social-links-menu',
+    'depth' => 1,
+    'link_before' => '<span class="screen-reader-text">',
+    'link_after' => '</span>' . captaintsubasa_get_svg(array('icon' => 'chain')),
+));?>
 				</nav><!-- .social-navigation -->
 			<?php endif;?>
 			<!-- Search -->
@@ -64,16 +64,20 @@
 		</nav><!-- End Menu -->
 
 <!-- Header -->
-		<div class="site-branding">
-			<?php
-/* Header Home */
-if (is_front_page() && is_home()): ?>
-				<p>Carrousel</p>
-			<!-- Header Post, Page -->
-			<?php elseif (is_single() || is_page()): ?>
-				<p class="header_title"><?php single_post_title();?></p>
-			<?php endif;?>
 
-		</div><!-- .site-branding -->
+			<!-- Header Post, Page -->
+			<?php if (is_single() || is_page()): ?>
+			<div class="page_infos">
+				
+				<?php $categories = get_the_category();
+					if ( ! empty( $categories ) ): ?>
+    					<p><a  class="page_category" href="<?=  get_category_link( $categories[0]->term_id );?>"><?= $categories[0]->name ?></a></p>
+					<?php endif ?>
+				<h1 class="page_title"><?php single_post_title();?></h1>
+				<?php if (is_single()) : ?>
+				<p class="page_date"><?= get_the_date() ; ?></p>
+				<?php endif;?>
+			</div>
+			<?php endif;?>
 	</header><!-- #masthead -->
 	<div id="content" class="site-content">
